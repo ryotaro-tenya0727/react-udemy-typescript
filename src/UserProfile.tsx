b42c2ac674220c01272d0a2c2b55bdd2bce55668
中山遼太郎 <71915489+ryotaro-tenya0727@users.noreply.github.com>
2022-01-18 10:39:40 +0900

    【追加】オプショナルチェイニングで安全なコードを書く

---
+++ src/UserProfile.tsx
@@ -0,0 +1,17 @@
+import { VFC } from "react";
+import { User } from "./types/user";
+
+type Props = {
+  user: User;
+};
+
+export const UserProfile: VFC<Props> = ({ user }) => {
+  return (
+    <dl>
+      <dt>名前</dt>
+      <dd>{user.name}</dd>
+      <dt>趣味</dt>
+      <dd>{user.hobbies?.join(" / ")}</dd>
+    </dl>
+  );
+};
